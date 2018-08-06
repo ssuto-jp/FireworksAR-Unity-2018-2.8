326fa72367ab03b061241857a5f3b4c2c9e867d1
suto <ss.time2531@gmail.com>
2018-08-07 00:46:03 +0900

    ボタンを配置

---
+++ Assets/Scripts/CameraViewController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraViewController : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("SaveViewScene");
+    }
+}
