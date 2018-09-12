4dcd6cff2ca60a85aa7206e80955d359fcef0807
suto <ss.time2531@gmail.com>
2018-09-13 03:45:36 +0900

    花火エフェクトをARで表示

--- Assets/Scripts/FireworkButton.cs
+++ Assets/Scripts/FireworkButton.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
 
 public class FireworkButton : MonoBehaviour
 {
     [SerializeField] private AppState _buttonType;
+    [SerializeField] private GameObject[] fireworkEffects;
     [SerializeField] private Text text;
 
     private Subject<AppState> _onClickedButton = new Subject<AppState>();
@@ -20,16 +22,31 @@ public class FireworkButton : MonoBehaviour
 
     public void ChangeFirework(AppState currentState)
     {
-        switch (currentState)
+        StartCoroutine(ChangeFireworkCoroutine(currentState));
+    }
+
+    private IEnumerator ChangeFireworkCoroutine(AppState state)
+    {
+        foreach (var effects in fireworkEffects)
+        {
+            effects.SetActive(false);
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        switch (state)
         {
             case AppState.Normal:
                 text.text = "Normal";
+                fireworkEffects[0].SetActive(true);
                 break;
             case AppState.Star:
                 text.text = "Star";
+                fireworkEffects[1].SetActive(true);
                 break;
             case AppState.Line:
                 text.text = "Line";
+                fireworkEffects[2].SetActive(true);
                 break;
         }
     }
